589d1a5f2df1034352cce38875871f5d19b3a722
MariaSavira <mariasavira2006@gmail.com>
2025-09-29 08:07:11 +0700

    Pemrograman Web Jobsheet 5.2 - Fungsi

---
+++ Pemrograman Web/Jobsheet 5 - PHP 2/Jobsheet 5.2 - Fungsi/recursive_2.php
@@ -0,0 +1,16 @@
+<?php
+for ($i = 1; $i <= 25; $i++){
+    echo "Perulangan ke-{$i} <br>";
+}
+
+echo "<hr>";
+
+function tampilkanAngka (int $jumlah, int $indeks = 1){
+    echo "Perulangan ke-{$indeks} <br>";
+    
+    if ($indeks < $jumlah){
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+tampilkanAngka(20);
+?>
