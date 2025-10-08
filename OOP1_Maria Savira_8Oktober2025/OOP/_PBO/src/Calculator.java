c2392ae2231354c4c9726e61ab83bb99efd14043
MariaSavira <mariasavira2006@gmail.com>
2025-10-08 13:48:02 +0700

    Tugas OOP Bu Mentari

---
+++ OOP1_Maria Savira_8Oktober2025/OOP/_PBO/src/Calculator.java
@@ -0,0 +1,9 @@
+package dataStructure;
+
+public class Calculator {
+     // Method untuk menjumlahkan dua bilangan bulat 
+    public int add(int a, int b) { 
+        // Mengembalikan hasil penjumlahan a dan b 
+        return a + b; 
+    }
+}
